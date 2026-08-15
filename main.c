94790d164efff61456d2e38a0cacfc1d713a4910
kzaknsok <eightymurasame@gmail.com>
2026-08-15 15:59:49 +0900

    全体の見直し

--- main.c
+++ main.c
@@ -1,28 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <time.h>
 #include <emscripten.h>
 
-// JavaScript から呼び出せるように export 指定
 EMSCRIPTEN_KEEPALIVE
 void init_rand() {
     srand((unsigned int)time(NULL));
 }
 
-// 選択肢のインデックスをシャッフル（Fisher-Yates アルゴリズム）
+// 配列のシャッフル（JavaScript側のInt32Arrayを直接操作）
 EMSCRIPTEN_KEEPALIVE
-void shuffle_indices(int *array, int n) {
-    if (n <= 1) return;
-    for (int i = n - 1; i > 0; i--) {
+void shuffle_array(int *arr, int size) {
+    if (size <= 1) return;
+    for (int i = size - 1; i > 0; i--) {
         int j = rand() % (i + 1);
-        int temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
 
-// 0 〜 total_count-1 からランダムなインデックスを取得
+// ランダムなインデックスを取得
 EMSCRIPTEN_KEEPALIVE
 int get_random_index(int total_count) {
     if (total_count <= 0) return 0;
